6768168e67058098527e3668cc7e2257e32c385d
Rishikesh Vedpathak <rishikeshvedpathak@gmail.com>
2021-05-26 02:17:00 +0530

    async actions

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,27 @@
 import create from "zustand";
+import { devtools } from "zustand/middleware";
 
 type BearState = {
   bears: number;
   increasePopulation: (by?: number | null) => void;
   removeAllBears: () => void;
+  asyncAddBears: () => void;
 };
 
-export const useStore = create<BearState>((set) => ({
-  bears: 0,
-  increasePopulation: (count) =>
-    set((state) => ({ bears: state.bears + (count || 1) })),
-  removeAllBears: () => set({ bears: 0 }),
-}));
+export const useStore = create<BearState>(
+  devtools(
+    (set) => ({
+      bears: 0,
+      increasePopulation: (count) =>
+        set((state) => ({ bears: state.bears + (count || 1) })),
+      removeAllBears: () => set({ bears: 0 }),
+      asyncAddBears: async () => {
+        const result = await fetch("https://reqres.in/api/users?delay=5")
+          .then((res) => res.json())
+          .then((res) => res);
+        set({ bears: result.per_page });
+      },
+    }),
+    "MyStore"
+  )
+);
